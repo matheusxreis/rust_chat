1a06d228f003f0beea92659f06935bb0b190284d
Matheus Reis <matheus.reis081@gmail.com>
2023-10-07 19:47:27 -0300

    sending new messages to connected users

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use serde::{Serialize, Deserialize};
 
 
 const ADDRESS:&str = "127.0.0.1:3030";
-
+struct UserData(SocketAddr, TcpStream);
 
 #[derive(Serialize, Deserialize)]
 struct InMemoryDB {
@@ -30,25 +30,25 @@ impl User {
 }
 
 
+
   
 fn main() {
 
     let listener = TcpListener::bind(ADDRESS).expect("Err to bind");
 
     println!("You can connect in {}", ADDRESS);
+    let available_streams:Arc<Mutex<Vec<UserData>>>=Arc::new(Mutex::<Vec<UserData>>::new(vec![]));
 
-
-    let messages = Arc::new(Mutex::<Vec<String>>::new(vec![]));
 
     for stream in listener.incoming() {
 
-        let msgs = messages.clone();
+
+        let available = available_streams.clone();
 
         let _: thread::JoinHandle<()> = thread::spawn(move || {
             println!("New thread");
-            
             println!("Connection was estabileshed");
-            handle_stream(stream.expect("msg"), msgs);    
+            handle_stream(stream.expect("msg"),  available);    
         });
         // let _ = t.join();
         }
@@ -59,8 +59,19 @@ fn main() {
 
 
 
-fn handle_stream(stream: TcpStream, messages: Arc<Mutex<Vec<String>>>) {
+fn handle_stream(
+    stream: TcpStream,
+    available_streams: Arc<Mutex<Vec<UserData>>>
+) {
 
+    {
+        let mut av = available_streams.lock().unwrap();
+        av.push(
+            UserData(stream.peer_addr().unwrap(), stream.try_clone().unwrap())
+        )
+
+    }
+    
     let mut writer: BufWriter<&TcpStream> = BufWriter::new( &stream);
     let mut reader: BufReader<&TcpStream> = BufReader::new(&stream);
 
@@ -85,7 +96,6 @@ fn handle_stream(stream: TcpStream, messages: Arc<Mutex<Vec<String>>>) {
     
      while let Ok(Some(s)) = read() {
         // I only must continue in case of user return his name
-
         
         let user = match  handle_user(String::from(s.trim()), stream.peer_addr().expect("failed to get local addr")) {
             Some(user) => user,
@@ -98,29 +108,56 @@ fn handle_stream(stream: TcpStream, messages: Arc<Mutex<Vec<String>>>) {
         };
 
 
+
         loop {
 
-         
-
-            write(String::from("> "));
-    
-            let r = read();
-    
-            if let Ok(Some(s)) = r {
+            write(String::from("> " ));
+            if let Ok(Some(s)) = read() {                
                 let msg = format!("<{}>: {}", user.name, s);
-               
-                println!("{}", msg);
-            }else {
-                println!("Fails to read {:#?}", r);
-            };
+                send_messages(msg, available_streams.clone(), stream.peer_addr().unwrap());
+
+              
+            }
         
         }
     }
     
 }
 
-fn send_messages() {
+fn send_messages(
+    current_message: String,
+    available_streams: Arc<Mutex<Vec<UserData>>>,
+    addr: SocketAddr
+) {
 
+    let streams = available_streams.lock().unwrap();
+    for data in streams.iter() {
+
+            // Itering all connected streams and send the message for all them, except by who sent
+
+            if data.0 != addr {
+            let mut writer = BufWriter::new(&data.1);
+            let _ = writer.write(current_message.as_bytes()).expect("Err in write");
+            let _ = writer.write("> ".as_bytes()).expect("Err in write");
+            let _ = writer.flush().expect("Err in flush writer");
+            
+            }
+            
+    }
+}
+
+fn get_db() -> InMemoryDB {
+    let reader = fs::read_to_string(Path::new("users.json")).expect("Failed to read file");
+
+    let in_memory_db:InMemoryDB = if !reader.is_empty()
+    { serde_json::from_str(&reader).expect("Failed to convert to json") } 
+   else 
+   {
+       let r = r#"{"users":[]}"#;
+       serde_json::from_str(r).expect("Failed to convert to json") 
+   };
+
+    in_memory_db
 }
 
 fn handle_user(name: String, addr: SocketAddr) -> Option<User> {
@@ -134,17 +171,7 @@ fn handle_user(name: String, addr: SocketAddr) -> Option<User> {
     .expect("Error in open file");
 
 
-    let reader = fs::read_to_string(Path::new("users.json")).expect("Failed to read file");
-
-    
-    let mut in_memory_db:InMemoryDB = if !reader.is_empty()
-     { serde_json::from_str(&reader).expect("Failed to convert to json") } 
-    else 
-    {
-        let r = r#"{"users":[]}"#;
-        serde_json::from_str(r).expect("Failed to convert to json") 
-    };
-
+    let mut in_memory_db = get_db();
 
     if let Some(_) = in_memory_db.users.iter().find(|x| x.name == name) {
             return None
