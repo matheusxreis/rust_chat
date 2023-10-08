51ab992252df3064c680c4e2813ca37e08ec22e2
Matheus Reis <matheus.reis081@gmail.com>
2023-10-07 21:17:10 -0300

    finishing connection and letting more friendly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::{
     thread,  
     net::{TcpListener, TcpStream, SocketAddr}, 
-    io::{BufRead, BufReader, Write, BufWriter}, fs::{self, File, OpenOptions}, path::Path, sync::{mpsc, Arc, Mutex}
+    io::{BufRead, BufReader, Write, BufWriter}, fs::{self, File, OpenOptions}, path::Path, sync::{mpsc, Arc, Mutex}, process, ops::Index
 };
 
 use serde::{Serialize, Deserialize};
 
 
 
-const ADDRESS:&str = "127.0.0.1:3030";
+const ADDRESS:&str = "0.0.0.0:3030";
+const ESCAPE_WORD: &str = "/q";
+// const ESCAPE_WINDOWS = "\n\r";
 struct UserData(SocketAddr, TcpStream);
 
 #[derive(Serialize, Deserialize)]
@@ -37,24 +39,25 @@ fn main() {
     let listener = TcpListener::bind(ADDRESS).expect("Err to bind");
 
     println!("You can connect in {}", ADDRESS);
+    
     let available_streams:Arc<Mutex<Vec<UserData>>>=Arc::new(Mutex::<Vec<UserData>>::new(vec![]));
 
 
     for stream in listener.incoming() {
 
-
         let available = available_streams.clone();
 
-        let _: thread::JoinHandle<()> = thread::spawn(move || {
+        let t: thread::JoinHandle<()> = thread::spawn(move || {
             println!("New thread");
             println!("Connection was estabileshed");
-            handle_stream(stream.expect("msg"),  available);    
+            handle_stream(stream.expect("msg"),  available); 
+            return;
         });
         // let _ = t.join();
         }
-        
-  
 
+        drop(listener)
+    
 }
 
 
@@ -64,13 +67,7 @@ fn handle_stream(
     available_streams: Arc<Mutex<Vec<UserData>>>
 ) {
 
-    {
-        let mut av = available_streams.lock().unwrap();
-        av.push(
-            UserData(stream.peer_addr().unwrap(), stream.try_clone().unwrap())
-        )
-
-    }
+   
     
     let mut writer: BufWriter<&TcpStream> = BufWriter::new( &stream);
     let mut reader: BufReader<&TcpStream> = BufReader::new(&stream);
@@ -90,17 +87,48 @@ fn handle_stream(
         };
     };
 
-    write(String::from("Welcome to the server\n"));
+    write(String::from("============ Welcome to the server ===============\n\r"));
+    write(String::from("============ You can: ============================\n\r"));
+    write(String::from("1. Wait for someone who talking\n\r"));
+    write(String::from("2. Typoe '/q' to leave\n\r"));
+
+
     write(String::from("Say your name: "));
 
     
      while let Ok(Some(s)) = read() {
         // I only must continue in case of user return his name
         
+
         let user = match  handle_user(String::from(s.trim()), stream.peer_addr().expect("failed to get local addr")) {
-            Some(user) => user,
+            Some(user) => {
+
+                {
+                    let mut av = available_streams.lock().unwrap();
+                    av.push(
+                        UserData(stream.peer_addr().unwrap(), stream.try_clone().unwrap())
+                    );
+
+                    let len = av.len() - 1;
+
+                    if  len == 0 {
+                        write(format!("*********** Only you is connected ***********\n\r"));
+
+                    }else {
+                        write(format!("*********** There is {} users current connected ***********\n\r", len));
+
+                    }
+
+                }
+                let msg = format!("*********** {} logged in =D ***********\n\r", user.name);
+                send_messages(msg, available_streams.clone(), stream.peer_addr().unwrap());
+
+                user
+            },
             _ => { 
-                write(String::from("User already logged in\n"));    
+
+                
+                write(String::from("User already logged in\n\r"));    
                 write(String::from("Type another name: "));    
 
                 continue;
@@ -114,12 +142,27 @@ fn handle_stream(
             write(String::from("> " ));
             if let Ok(Some(s)) = read() {                
                 let msg = format!("<{}>: {}", user.name, s);
-                send_messages(msg, available_streams.clone(), stream.peer_addr().unwrap());
+
+                if s.trim() == ESCAPE_WORD {
+                    
+                    let msg = format!("*********** {} logged out ***********\n\r", user.name);
+                    send_messages(msg, available_streams.clone(), stream.peer_addr().unwrap());
+                    let mut av = available_streams.lock().unwrap();
+                    if let Some(index) = av.iter().position(|x| x.0 == stream.peer_addr().unwrap()) {
+                        av.swap_remove(index);
+                    }
+                    write("Please, press Ctrl + C to finish connection".to_string());
+                    break;
+                }else {
+                    send_messages(msg, available_streams.clone(), stream.peer_addr().unwrap());
+                }
+
 
               
             }
         
         }
+         break;
     }
     
 }
